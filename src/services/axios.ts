440e27b2dba8a4dab4a43dbfb564dfd1c835c2f3
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2023-02-21 19:03:31 -0300

    env add

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,11 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.baseURL,
 })
-
-// import axios from 'axios'
-
-// export const api = axios.create({
-//   baseURL: 'https://worldtrip-silk.vercel.app/api',
-// })
